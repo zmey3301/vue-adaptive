c8f2c06ddd4934d823036e3ebd1722111b6cd2f9
SoulString <zmey3301@gmail.com>
2018-03-17 17:24:13 +0400

    Optimization, bugfixes, release
    add: throttle for window resize event
    add: jsdoc comments
    add: minified dist
    add: rem equality test
    fix: device equality test

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
-// Copyright © 2017-2018 Mikhail Perelygin <zmey3301@gmail.com>
-// Huge thanks to Koshevarov Sergey <gondragos@gmail.com> for plugin idea
-// Program is distributed under the terms of the GNU Affero General Public License
-import _ from 'lodash'
-import Adaptive from './adaptive'
+/*******************************************************************************
+ * Copyright © 2017-2018 Mikhail Perelygin <zmey3301@gmail.com>
+ * Huge thanks to Koshevarov Sergey <gondragos@gmail.com> for plugin idea
+ * Program is distributed under the terms of the GNU Affero General Public License
+ *
+ * @date         17.3.2018
+ * @license     AGPL-3.0
+ ******************************************************************************/
 
+import Adaptive from './adaptive'
 export default {
+	/**
+	 * Vue plugin install function
+	 * @param Vue
+	 * @param config
+	 */
 	install (Vue, config) {
-		const defaultConfig = {
-			if			: false,
-			element		: window,
-			rem			: false,
-			from		: false,
-			to			: false,
-			base		: false,
-			k			: 1,
-			setDevice	: false
-		}
-		let deviceList = {}
-		for (let device in config) {
-			if (config.hasOwnProperty(device)) {
-				_.defaults(config[device], defaultConfig)
-				device = device.split(':')[0]
-				if (!deviceList.hasOwnProperty(device)) deviceList[device] = false
-			}
-		}
-		let adaptive = new Adaptive(config, deviceList)
+		let adaptive = new Adaptive(config)
 		Vue.mixin({
 			beforeCreate () {
 				this.$adaptive = adaptive
